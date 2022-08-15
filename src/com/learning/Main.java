4d43303ef8e6b80d121dd26704101be9bd3d02dc
SHIVAM SRIVASTAVA <shivamsrivastava@SHIVAMs-MacBook-Air.local>
2022-08-15 19:28:30 +0530

    Initial commit

---
+++ src/com/learning/Main.java
@@ -0,0 +1,21 @@
+package com.learning;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyStack s = new MyStack();
+
+
+        s.push(3);
+        s.push(5);
+        s.getMin();
+        s.push(2);
+        s.push(1);
+        s.getMin();
+        s.pop();
+        s.getMin();
+        s.pop();
+        s.peek();
+    }
+}
